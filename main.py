c803e55a66ecb7e8b8e7581d7c21f595b9a86d79
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-16 13:45:07 +0100

    add access to Draw and GameLogic classes

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from tkinter import *
+from board import Board
 
 root = Tk()
 
-root.configure(bg="#6cc2f0")
+root.configure(bg="black")
 root.geometry("600x550")
 root.title("Tic Tac Toe")
 root.resizable(False, False)
 
-title_frame = Frame(root, bg="#6cc2f0",
- width=600, height=100)
+title_frame = Frame(root, bg="black", width=600, height=100)
 title_frame.place(x=0, y=0)
 
-right_frame = Frame(root, bg="#6cc2f0",
-                    width=150, height=450)
-right_frame.place(x=450,y=100)
+right_frame = Frame(root, bg="black", width=150, height=450)
+right_frame.place(x=450, y=100)
 
-board_frame = Frame(root, bg="#6cc2f0",
-                    width=450, height=450
-                    )
+board_frame = Frame(root, bg="black", width=450, height=450)
 board_frame.place(x=0, y=100)
+
+b = Board(board_frame)
+
 root.mainloop()
