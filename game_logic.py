ebbde4914d1e8049c2e796cea241af1010effe08
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-16 16:40:55 +0100

    make working check if tie and check if win

--- game_logic.py
+++ game_logic.py
@@ -5,34 +5,34 @@ class GameLogic:
     def check_if_win(self):
         for i in range(3):
             if (
-                    self.buttons[i][0]["text"] != " "
-                    and self.buttons[i][0]["text"]
-                    == self.buttons[i][1]["text"]
-                    == self.buttons[i][2]["text"]
+                self.buttons[i][0]["text"] != " "
+                and self.buttons[i][0]["text"]
+                == self.buttons[i][1]["text"]
+                == self.buttons[i][2]["text"]
             ):
                 return True
             elif (
-                    self.buttons[0][i]["text"] != " "
-                    and self.buttons[0][i]["text"]
-                    == self.buttons[1][i]["text"]
-                    == self.buttons[2][i]["text"]
+                self.buttons[0][i]["text"] != " "
+                and self.buttons[0][i]["text"]
+                == self.buttons[1][i]["text"]
+                == self.buttons[2][i]["text"]
             ):
                 return True
         if (
-                self.buttons[0][2]["text"] != " "
-                and self.buttons[0][2]["text"]
-                == self.buttons[1][1]["text"]
-                == self.buttons[2][0]["text"]
+            self.buttons[0][2]["text"] != " "
+            and self.buttons[0][2]["text"]
+            == self.buttons[1][1]["text"]
+            == self.buttons[2][0]["text"]
         ):
             return True
         elif (
-                self.buttons[0][0]["text"] != " "
-                and self.buttons[0][0]["text"]
-                == self.buttons[1][1]["text"]
-                == self.buttons[2][2]["text"]
+            self.buttons[0][0]["text"] != " "
+            and self.buttons[0][0]["text"]
+            == self.buttons[1][1]["text"]
+            == self.buttons[2][2]["text"]
         ):
-            return True
-
+            diagonal = "left_to_right_down"
+            return True, diagonal
 
     def check_if_tie(self):
         for i in range(3):
@@ -40,6 +40,3 @@ class GameLogic:
                 if self.buttons[i][j]["text"] == " ":
                     return False
         return True
-
-
-   
