cb27e823eb8db78ed1dd34c91c330e357f1bc974
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-16 16:41:38 +0100

    merge three files and make file clearer to read

--- board.py
+++ board.py
@@ -1,4 +1,6 @@
 from tkinter import Button, Canvas, messagebox
+from game_logic import GameLogic
+from drawing import Draw
 
 
 class Board:
@@ -6,6 +8,8 @@ class Board:
         self.localization = localization
         self.current_player = "X"
         self.buttons = [[" " for _ in range(3)] for _ in range(3)]
+        self.canvas = Canvas(self.localization, width=100, height=100, background="black",highlightthickness=0)
+        self.drawing = Draw(self.canvas)
 
         for i in range(3):
             for j in range(3):
@@ -15,6 +19,7 @@ class Board:
                     height=10,
                     width=20,
                     command=lambda row=i, col=j: self.make_move(row, col),
+                    background="black",
                 )
                 self.buttons[i][j].grid(row=i, column=j)
 
@@ -22,12 +27,13 @@ class Board:
         self.row = row
         self.col = col
         if self.buttons[row][col]["text"] == " ":
-            canvas = Canvas(self.localization, width=100, height=100)
+            canvas = Canvas(self.localization, width=100, height=100, background="black", highlightthickness=0)
             self.canvas = canvas
+            drawing = Draw(self.canvas)
             if self.current_player == "X":
-                self.buttons[row][col].config(text=self.draw_X())
+                self.buttons[row][col].config(text=drawing.draw_X())
             else:
-                self.buttons[row][col].config(text=self.draw_0())
+                self.buttons[row][col].config(text=drawing.draw_0())
             canvas.grid(row=row, column=col)
 
             if self.check_if_win():
@@ -37,49 +43,10 @@ class Board:
 
             self.current_player = "O" if self.current_player == "X" else "X"
 
-    def draw_X(self):
-        self.canvas.create_line(0, 0, 95, 95, width=15)
-        self.canvas.create_line(0, 95, 95, 0, width=15)
-        return "X"
-
-    def draw_0(self):
-        self.canvas.create_oval(10, 10, 95, 95, width=15)
-        return "0"
-
     def check_if_win(self):
-        for i in range(3):
-            if (
-                self.buttons[i][0]["text"] != " "
-                and self.buttons[i][0]["text"]
-                == self.buttons[i][1]["text"]
-                == self.buttons[i][2]["text"]
-            ):
-                return True
-            elif (
-                self.buttons[0][i]["text"] != " "
-                and self.buttons[0][i]["text"]
-                == self.buttons[1][i]["text"]
-                == self.buttons[2][i]["text"]
-            ):
-                return True
-        if (
-            self.buttons[0][2]["text"] != " "
-            and self.buttons[0][2]["text"]
-            == self.buttons[1][1]["text"]
-            == self.buttons[2][0]["text"]
-        ):
-            return True
-        elif (
-            self.buttons[0][0]["text"] != " "
-            and self.buttons[0][0]["text"]
-            == self.buttons[1][1]["text"]
-            == self.buttons[2][2]["text"]
-        ):
-            return True
+        game_logic = GameLogic(self.buttons)
+        return game_logic.check_if_win()
 
     def check_if_tie(self):
-        for i in range(3):
-            for j in range(3):
-                if self.buttons[i][j]["text"] == " ":
-                        return False
-        return True
+        game_logic = GameLogic(self.buttons)
+        return game_logic.check_if_tie()
