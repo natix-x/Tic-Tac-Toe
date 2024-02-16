f3ae6c4faf87aaccc89bf1785b54fb4999cee468
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-16 13:43:30 +0100

    create GameLogic class

---
+++ game_logic.py
@@ -0,0 +1,45 @@
+class GameLogic:
+    def __init__(self, buttons):
+        self.buttons = buttons
+
+    def check_if_win(self):
+        for i in range(3):
+            if (
+                    self.buttons[i][0]["text"] != " "
+                    and self.buttons[i][0]["text"]
+                    == self.buttons[i][1]["text"]
+                    == self.buttons[i][2]["text"]
+            ):
+                return True
+            elif (
+                    self.buttons[0][i]["text"] != " "
+                    and self.buttons[0][i]["text"]
+                    == self.buttons[1][i]["text"]
+                    == self.buttons[2][i]["text"]
+            ):
+                return True
+        if (
+                self.buttons[0][2]["text"] != " "
+                and self.buttons[0][2]["text"]
+                == self.buttons[1][1]["text"]
+                == self.buttons[2][0]["text"]
+        ):
+            return True
+        elif (
+                self.buttons[0][0]["text"] != " "
+                and self.buttons[0][0]["text"]
+                == self.buttons[1][1]["text"]
+                == self.buttons[2][2]["text"]
+        ):
+            return True
+
+
+    def check_if_tie(self):
+        for i in range(3):
+            for j in range(3):
+                if self.buttons[i][j]["text"] == " ":
+                    return False
+        return True
+
+
+   
