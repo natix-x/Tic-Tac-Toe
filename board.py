8f5fe50d9aa3e7c257dcfcd93bdecb8d2e872e9a
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-18 01:05:38 +0100

    connect better with GameLogic & add highlight module to highlight winning line

--- board.py
+++ board.py
@@ -8,8 +8,7 @@ class Board:
         self.localization = localization
         self.current_player = "X"
         self.buttons = [[" " for _ in range(3)] for _ in range(3)]
-        self.canvas = Canvas(self.localization, width=100, height=100, background="black",highlightthickness=0)
-        self.drawing = Draw(self.canvas)
+        self.canvases = [[None for _ in range(3)] for _ in range(3)]
 
         for i in range(3):
             for j in range(3):
@@ -24,29 +23,38 @@ class Board:
                 self.buttons[i][j].grid(row=i, column=j)
 
     def make_move(self, row, col):
-        self.row = row
-        self.col = col
         if self.buttons[row][col]["text"] == " ":
-            canvas = Canvas(self.localization, width=100, height=100, background="black", highlightthickness=0)
-            self.canvas = canvas
+            self.canvases[row][col] = Canvas(
+                self.localization,
+                width=145,
+                height=145,
+                background="black",
+                highlightthickness=0,
+            )
+            self.canvas = self.canvases[row][col]
             drawing = Draw(self.canvas)
             if self.current_player == "X":
                 self.buttons[row][col].config(text=drawing.draw_X())
             else:
                 self.buttons[row][col].config(text=drawing.draw_0())
-            canvas.grid(row=row, column=col)
 
-            if self.check_if_win():
+            self.canvases[row][col].grid(row=row, column=col)
+
+            game_logic = GameLogic(self.buttons)
+            winning_combination = game_logic.check_if_win()
+            if winning_combination:
+                self.highlight_winning_combination(winning_combination)
                 messagebox.showinfo("Game Over", f"Player {self.current_player} wins!")
-            elif self.check_if_tie():
-                messagebox.showinfo("Game Over", "TIE!")
+            elif game_logic.check_if_tie():
+                messagebox.showinfo("Game Over", "Tie!")
 
             self.current_player = "O" if self.current_player == "X" else "X"
 
-    def check_if_win(self):
-        game_logic = GameLogic(self.buttons)
-        return game_logic.check_if_win()
+    def highlight_winning_combination(self, combination):
+        if combination:
+            for row, col in combination:
+                self.canvases[row][col].config(bg="red")
 
-    def check_if_tie(self):
-        game_logic = GameLogic(self.buttons)
-        return game_logic.check_if_tie()
+
+
+
