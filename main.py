54144031d9441f758812f391ef680221c240c55f
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-20 15:54:08 +0100

    define main function: tic_tac_toe()

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from tkinter import *
 from board import Board
+from menu import Menu
 
-root = Tk()
 
-root.configure(bg="black")
-root.geometry("600x550")
-root.title("Tic Tac Toe")
-root.resizable(False, False)
+def tic_tac_toe():
+    root = Tk()
 
-title_frame = Frame(root, bg="black", width=600, height=100)
-title_frame.place(x=0, y=0)
+    root.configure(bg="black")
+    root.geometry("600x550")
+    root.title("Tic Tac Toe")
+    root.resizable(False, False)
 
-right_frame = Frame(root, bg="black", width=150, height=450)
-right_frame.place(x=450, y=100)
+    title_frame = Frame(root, bg="black", width=450, height=100)
+    title_frame.place(x=0, y=0)
 
-board_frame = Frame(root, bg="black", width=450, height=450)
-board_frame.place(x=0, y=100)
+    right_frame = Frame(root, bg="black", width=150, height=600)
+    right_frame.place(x=450, y=100)
 
-b = Board(board_frame)
+    board_frame = Frame(root, bg="black", width=450, height=450)
+    board_frame.place(x=0, y=100)
 
-root.mainloop()
+    right_menu = Menu(right_frame)
+    board = Board(board_frame, right_menu)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    tic_tac_toe()
