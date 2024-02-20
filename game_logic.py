77b561993b2541a93d25227dd4a519514d46e5f2
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-20 16:56:15 +0100

    add descriptions & make check_if_win more flexible

--- game_logic.py
+++ game_logic.py
@@ -6,52 +6,53 @@ class GameLogic:
         self.buttons = buttons
 
     def check_if_win(self):
+        """
+        checks if won.
+        Possible winning combinations:
+        1. Three same symbols in one row
+        2. Three same symbols in one column
+        3. Three same symbols in one diagonal
+        :return: winning combination
+        """
         for i in range(3):
+            for j in range(3):
+                if (
+                        self.buttons[i][j]["text"] != " "
+                        and self.buttons[i][j]["text"]
+                        == self.buttons[i][(j + 1) % 3]["text"]
+                        == self.buttons[i][(j + 2) % 3]["text"]
+                ):
+                    return [(i, 0), (i, 1), (i, 2)]  # return winning row
+                elif (
+                        self.buttons[i][j]["text"] != " "
+                        and self.buttons[i][j]["text"]
+                        == self.buttons[(i + 1) % 3][j]["text"]
+                        == self.buttons[(i + 2) % 3][j]["text"]
+                ):
+                    return [(0, j), (1, j), (2, j)]  # return winning column
+                elif (
+                        self.buttons[i][j]["text"] != " "
+                        and self.buttons[i][j]["text"]
+                        == self.buttons[(i + 1) % 3][(j + 1) % 3]["text"]
+                        == self.buttons[(i + 2) % 3][(j + 2) % 3]["text"]
+                ):
+                    return [(i, j), (i + 1, j + 1), (i + 2, j + 2)]  # return first winning diagonal
             if (
-                self.buttons[i][0]["text"] != " "
-                and self.buttons[i][0]["text"]
-                == self.buttons[i][1]["text"]
-                == self.buttons[i][2]["text"]
+                    self.buttons[i][(2 - i)]["text"] != " "
+                    and self.buttons[i][(2 - i)]["text"]
+                    == self.buttons[(i + 1) % 3][2 - (i + 1) % 3]["text"]
+                    == self.buttons[(i + 2) % 3][2 - (i + 2) % 3]["text"]
             ):
-                return [(i, 0), (i, 1), (i, 2)]
-            elif (
-                self.buttons[0][i]["text"] != " "
-                and self.buttons[0][i]["text"]
-                == self.buttons[1][i]["text"]
-                == self.buttons[2][i]["text"]
-            ):
-                return [(0, i), (1, i), (2, i)]
-        if (
-            self.buttons[0][0]["text"] != " "
-            and self.buttons[0][0]["text"]
-            == self.buttons[1][1]["text"]
-            == self.buttons[2][2]["text"]
-        ):
-            return [(0, 0), (1, 1), (2, 2)]
-        elif (
-            self.buttons[0][2]["text"] != " "
-            and self.buttons[0][2]["text"]
-            == self.buttons[1][1]["text"]
-            == self.buttons[2][0]["text"]
-        ):
-            return [(0, 2), (1, 1), (2, 0)]
+                return [(0, 2), (1, 1), (2, 0)]  # return second winning diagonal
         return None
 
     def check_if_tie(self):
+        """
+        checks if tie
+        "tie" means no same three symbols in one row/column/diagonal
+        """
         for i in range(3):
             for j in range(3):
                 if self.buttons[i][j]["text"] == " ":
                     return False
         return True
-
-    def restart_board(self, canvases):
-        for i in range(3):
-            for j in range(3):
-                self.buttons[i][j].config(text=" ")
-                if canvases[i][j] is not None:
-                    canvases[i][j].destroy()
-
-    def handle_restart(self, message, canvases):
-        restart = messagebox.askyesno("Game Over", f"{message} Do you want to restart?", icon='question',default='yes')
-        if restart:
-            self.restart_board(canvases)
